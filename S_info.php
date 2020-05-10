428286c45e9b076071d3a8d583a57740d0b2f321
Prashan Shrestha <53164770+ShresPrashan@users.noreply.github.com>
2020-05-10 17:06:19 +1000

    commit by Prashanna Shrestha
    This is a PHP file for the software page of the website

---
+++ S_info.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+include 'header.php';
+?>
+
+<?php
+
+    $conn = Open_Connection();
+   
+    $sql= "SELECT * FROM article where a_id = '2'";
+    $result = mysqli_query($conn, $sql);
+    $queryResult= mysqli_num_rows($result);
+    if ($queryResult>0) {
+        while ($row = mysqli_fetch_assoc($result)){
+	echo "<div>
+	<h3> ".$row['a_title']."</h3>
+	<p> ".$row['a_text']."</p>
+	</div>";
+}
+    }
+    
+    else{
+        echo 'No results found.';
+        echo ' Error message: No related information found.';
+    }
+
+
+?>
